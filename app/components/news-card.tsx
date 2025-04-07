696a372c0c422aeaf83f3d9a8d3d2289d887f699
bobbyjoe91 <--global>
2025-04-07 20:31:02 +0700

    feat: [PREM-13, 14] news - add slug and more news data - iterate NewsCard on data

--- app/components/news-card.tsx
+++ app/components/news-card.tsx
@@ -4,10 +4,11 @@ import { Link } from "react-router";
 interface NewsCardProps {
   image: string | null;
   title: string;
+  slug: string;
   content: string;
 }
 
-const NewsCard: React.FC<NewsCardProps> = ({ image, title, content }) => {
+const NewsCard: React.FC<NewsCardProps> = ({ image, title, slug, content }) => {
   return (
     <div className="flex flex-col lg:flex-row lg:max-w-5xl rounded-2xl mb-10">
       <div className="flex justify-center lg:justify-start h-44 w-full lg:w-2/6 m-2 rounded-2xl overflow-hidden">
@@ -21,7 +22,8 @@ const NewsCard: React.FC<NewsCardProps> = ({ image, title, content }) => {
         <h1 className=" font-semibold mb-4">{title}</h1>
         <p className="text-sm lg:mr-12">{content.substring(0, 150)}...</p>
         <Link
-          to={"#"}
+          viewTransition
+          to={`/news/${slug}`}
           className="rounded-md border-2 font-semibold mb-1 text-lg py-1 px-4 shadow-md shadow-gray-400 mt-14 lg:mt-auto"
         >
           READ MORE
--- app/routes/news.tsx
+++ app/routes/news.tsx
@@ -1,6 +1,8 @@
 import NewsCard from "~/components/news-card";
 import type { Route } from "./+types/news";
 
+import newsData from "~/data/news.json";
+
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
@@ -8,28 +10,35 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-const lorem = `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam alias, natus, sequi velit rerum soluta neque impedit cum at eaque officia blanditiis deserunt labore ipsa distinctio ipsam doloribus veniam unde
-Et quaerat perspiciatis eum quam temporibus sapiente, sequi quidem aspernatur quis? Labore vero corrupti fugiat deleniti, sint temporibus ipsa? Incidunt nulla natus animi reiciendis magnam voluptates adipisci officia blanditiis dolorem.
-Sequi omnis quod at voluptates, eius qui ex debitis. Consequuntur ullam iure vitae sit doloremque totam quos, explicabo quo eius ad enim recusandae voluptatibus magni modi voluptates, obcaecati laborum laudantium?
-Ratione ea reiciendis maxime ex, odio tempore mollitia necessitatibus impedit eum error suscipit consequatur expedita quidem aliquam. Iusto esse dolore nihil voluptate magnam maiores temporibus! Corrupti alias aliquam architecto veritatis?
-Labore animi rerum quisquam eius`;
+export async function loader() {
+  const news = newsData; // TODO: replaced with Wordpress API call
+  return news;
+}
 
-export default function News() {
+type News = {
+  id: number;
+  image: string
+  title: string;
+  slug: string;
+  content: string;
+};
+
+export default function News({ loaderData }: Route.ComponentProps) {
   return (
     <div className="flex flex-grow flex-col items-center lg:gap-4 lg:items-start lg:px-28 px-12 pt-48 text-3xl font-light">
-      <NewsCard
-        image={"/samples/other-content.jpg"}
-        title="IFBEC NTB Gandeng Premium Syrup sebagai Sponsor Utama, Gelar Barista Competition 2024 di Mataram
-"
-        content={lorem}
-      />
-      <NewsCard
-        image={"/samples/other-content.jpg"}
-        title="Royal Malioboro by Aston Gelar Kompetisi Seni Memahat Buah dan Coffee Mixology Dalam Rangka Ulang Tahun Ke-4
-
-"
-        content={lorem}
-      />
+      {
+        loaderData.map((news: News) => {
+          return (
+            <NewsCard
+              key={news.id}
+              image={news.image}
+              title={news.title}
+              slug={news.slug}
+              content={news.content}
+            />
+          );
+        })
+      }
     </div>
   );
 }
