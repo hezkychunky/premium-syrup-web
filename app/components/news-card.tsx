6dd2d2c97f6cf06436862143f877eca3e57d5ef2
bobbyjoe91 <--global>
2025-04-08 21:57:58 +0700

    feat: [PREM-13, 14] news card - responsive news card

--- app/components/news-card.tsx
+++ app/components/news-card.tsx
@@ -10,21 +10,19 @@ interface NewsCardProps {
 
 const NewsCard: React.FC<NewsCardProps> = ({ image, title, slug, content }) => {
   return (
-    <div className="flex flex-col lg:flex-row lg:max-w-5xl rounded-2xl mb-10">
-      <div className="flex justify-center lg:justify-start h-44 w-full lg:w-2/6 m-2 rounded-2xl overflow-hidden">
-        <img
-          src={image ? image : ""}
-          alt="recipe category image"
-          className="rounded-2xl lg:w-full"
-        />
-      </div>
-      <div className="flex flex-col justify-center text-xl items-center lg:w-4/6 lg:items-start">
-        <h1 className=" font-semibold mb-4">{title}</h1>
-        <p className="text-sm lg:mr-12">{content.substring(0, 150)}...</p>
+    <div className="flex flex-col md:flex-row md:items-stretch lg:max-w-5xl rounded-2xl mb-10">
+      <img
+        src={image ?? ''}
+        alt="recipe category image"
+        className="h-full md:max-w-72 md:mr-2 rounded-2xl overflow-hidden"
+      />
+      <div className="flex flex-col justify-center text-xl items-center md:w-4/6 md:items-start">
+        <h1 className="font-semibold mt-4 md:mt-0">{title}</h1>
+        <p className="text-sm lg:max-w-120">{content.substring(0, 150)}...</p>
         <Link
           viewTransition
           to={`/news/${slug}`}
-          className="rounded-md border-2 font-semibold mb-1 text-lg py-1 px-4 shadow-md shadow-gray-400 mt-14 lg:mt-auto"
+          className="rounded-md border-1 font-semibold mb-1 md:text-sm lg:text-base py-1 px-4 shadow-md shadow-gray-400 mt-4 md:mt-auto"
         >
           READ MORE
         </Link>
