a2cff238f0b495bbd3f2debcbfbad3a2e30eec9a
bobbyjoe91 <bobby.cool00763@protonmail.com>
2025-06-25 22:10:14 +0700

    feat: carousel - infinite loop - reset timer on prev/next button click

--- app/components/carousel.tsx
+++ app/components/carousel.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 interface CarouselItem {
   image: string;
@@ -14,33 +14,82 @@ interface CarouselProps {
 }
 
 const Carousel: React.FC<CarouselProps> = ({ items }) => {
-  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
+
+  const [currentIndex, setCurrentIndex] = useState<number>(1);
+  const [isTransitioning, setIsTransitioning] = useState<boolean>(true);
+  const [carouselItems, _] = useState<CarouselItem[]>([
+    items[items.length - 1], // Start with the last item
+    ...items, // Add all items
+    items[0], // Add the first item at the end for looping
+  ]);
 
   // Automatically change slide every 3 seconds
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % items.length);
-    }, 3000);
+    resetTimer();
 
-    return () => clearInterval(interval);
+    return () => {
+      if (timerRef.current) {
+        clearInterval(timerRef.current);
+      }
+    };
   }, []); // Only run the effect once when the component mounts
 
+  useEffect(() => {
+    if (!isTransitioning) {
+      const timer = setTimeout(() => {
+        setIsTransitioning(true);
+      }, 50);
+
+      return () => clearTimeout(timer);
+    }
+  }, [isTransitioning]);
+
   const goToNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % items.length); // Loop to the first slide when going forward
-    console.info("clicked");
+    setCurrentIndex((prevIndex) => prevIndex + 1); // Loop to the first slide when going forward
+    setIsTransitioning(true);
+    resetTimer();
   };
 
   const goToPrevious = () => {
-    setCurrentIndex(
-      (prevIndex) => (prevIndex - 1 + items.length) % items.length
-    ); // Loop to the last slide when going backward
+    setCurrentIndex((prevIndex) => prevIndex - 1); // Loop to the last slide when going backward
+    setIsTransitioning(true);
+    resetTimer();
+  };
+
+  const onEdgeItems = () => {
+    if (currentIndex >= carouselItems.length - 1) {
+      setCurrentIndex(1); // Reset to first slide if at the end
+      setIsTransitioning(false);
+    }
+
+    if (currentIndex <= 0) {
+      setCurrentIndex(carouselItems.length - 2); // Reset to last slide if at the beginning
+      setIsTransitioning(false);
+    }
+  };
+
+  const resetTimer = () => {
+    if (timerRef.current) {
+      clearInterval(timerRef.current);
+    }
+
+    timerRef.current = setInterval(() => {
+      setCurrentIndex((prevIndex) => prevIndex + 1); // Loop to the first slide when going forward
+      setIsTransitioning(true);
+    }, 3000);
   };
 
   return (
     <div
       className="relative mx-auto overflow-hidden flex items-center justify-center transition-colors duration-700 ease-in-out"
       style={{
-        backgroundColor: items[currentIndex].bgColor || "transparent",
+        // show background color of the beginning or end item on fast clicks
+        backgroundColor: currentIndex <= 0
+          ? carouselItems[0]?.bgColor
+          : currentIndex >= carouselItems.length - 1
+          ? carouselItems[carouselItems.length - 1]?.bgColor
+          : carouselItems[currentIndex]?.bgColor, // Use background color of the first or last item
       }}
     >
       {
@@ -57,12 +106,13 @@ const Carousel: React.FC<CarouselProps> = ({ items }) => {
       }
 
       <div
-        className="flex transition-transform duration-700 ease-in-out"
+        className={`flex ${isTransitioning ? "transition-all duration-700 ease-in-out" : ""}`}
+        onTransitionEnd={onEdgeItems}
         style={{
           transform: `translateX(-${currentIndex * 100}%)`, // Slide effect
         }}
       >
-        {items.map((item, index) => (
+        {carouselItems.map((item, index) => (
           <div
             key={index}
             className="relative w-full flex-shrink-0 my-auto"
